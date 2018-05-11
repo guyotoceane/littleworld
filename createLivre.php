6de209396bc9bdeffaf401d2c3344dd6664f30b9
oguyot <guyot.oceane1@gmail.com>
2018-05-11 14:26:52 +0200

    ajax pour récup fiches

--- createLivre.php
+++ createLivre.php
@@ -21,12 +21,8 @@
 					</div>
 					<div class="row">
 						<div class="block-fiche px-3"> 
-							<ul class="d-flex justify-content-around">
-								<li><img src="images/page1.PNG"></li>
-								<li><img src="images/page1.PNG"></li>
-								<li><img src="images/page1.PNG"></li>
-								<li><img src="images/page1.PNG"></li>
-								<li><img src="images/page1.PNG"></li>
+							<ul class="d-flex justify-content-around" id="list-fiches">
+
 							</ul>
 						</div>
 					</div>
@@ -65,5 +61,58 @@
 </div>
 </section>
 <script src="js/less.min.js" ></script>
+<script src="js/jquery.js" ></script>
+
+<script>
+
+    $.ajax({
+        // chargement du fichier externe monfichier-ajax.php
+        url      : "http://localhost/littleworldBack/fiche/",
+        cache    : false,
+        type : "GET",
+        error    : function(request, error) { // Info Debuggage si erreur
+            alert("Erreur : responseText: "+request.responseText);
+        },
+        success  : function(data) {
+
+            data = JSON.parse(data);
+
+            console.log(data.length)
+
+
+            for(let i=0; i< data.length; i++){
+                let li = document.createElement('li');
+                console.log(data[i])
+
+                // li.innerText = data[i].nam_land
+
+                let img = document.createElement('img');
+
+                img.setAttribute('src', "./images/fiches_pays/"+data[i].nam_img);
+                img.style.width = '5em'
+
+                let text = document.createElement("p")
+
+                text.appendChild(document.createTextNode(data[i].nam_land))
+
+                text.style.textAlign = "center"
+
+                li.appendChild(img);
+
+                li.appendChild(text);
+
+                li.id = data[i].idt_lnd
+
+                document.getElementById('list-fiches').appendChild(li)
+
+            }
+
+        }
+    })
+
+</script>
+
+
+
 </body>
 </html>
