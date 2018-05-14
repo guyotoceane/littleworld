122722beb52381491b6cdd30758d259ad99d06b2
kioup <mftrookie@example.com>
2018-05-14 16:59:30 +0200

    Mis en page 1 effectué + 1/2 de la seconde

---
+++ ficheformat2.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+	<head lang="fr">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta charset="UTF-8">
+		<title>Littleworld</title>
+		<link rel="stylesheet/less" type="text/css" href="less/stylesFiche.less" />
+		<link rel="stylesheet" type="text/css" href="Bootstrap/css/bootstrap.min.css" />
+	</head>
+<body>
+    <div class="container fiche2">
+        <div class="row titre">
+            <h1>Allemagne</h1>
+        </div>
+        <div class="blockBleu">
+            <div class="row">
+                <h2>Histoire du pays</h2>
+            </div>
+            <div class="row">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore m. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.
+                </p>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="data-block">
+                        <p>Superficie : </p>
+                        <p></p>
+                        <p>Population : </p>
+                        <p></p>
+                        <p>Capitale : </p>
+                        <p></p>
+                        <p>Langue : </p>
+                        <p></p>
+                        <p>Monnaie : </p>
+                        <p></p>
+                        <p>Continent : </p>
+                        <p></p>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <img src="images/background.png" alt="image du pays">
+                </div>
+            </div>
+            <div class="row footer">
+            </div>
+        </div>
+    </div>
+<script src="js/less.min.js" ></script>
+<script src="js/jquery.js" ></script>
+</body>
+</html>
