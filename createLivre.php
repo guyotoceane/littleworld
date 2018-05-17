12a847233325693da2e9a97cf8562405b7aa9491
oguyot <guyot.oceane1@gmail.com>
2018-05-17 14:12:15 +0200

    drag & drop création de livres

--- createLivre.php
+++ createLivre.php
@@ -7,6 +7,36 @@
 		<link rel="stylesheet/less" type="text/css" href="less/styles.less" />
 		<link rel="stylesheet" type="text/css" href="Bootstrap/css/bootstrap.min.css" />
 	</head>
+    <style>
+        li{
+            list-style: none;
+        }
+
+        .gu-mirror {
+            position: fixed !important;
+            margin: 0 !important;
+            z-index: 9999 !important;
+            opacity: 0.8;
+            -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=80)";
+            filter: alpha(opacity=80);
+        }
+        .gu-hide {
+            display: none !important;
+        }
+        .gu-unselectable {
+            -webkit-user-select: none !important;
+            -moz-user-select: none !important;
+            -ms-user-select: none !important;
+            user-select: none !important;
+        }
+        .gu-transit {
+            opacity: 0.2;
+            -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=20)";
+            filter: alpha(opacity=20);
+        }
+
+    </style>
+
 <body>
 <?php include("header.php"); ?>
 <section>
@@ -15,28 +45,21 @@
 		<div class="row">
 			<?php include("aside.php"); ?> 
 			<div class="content col-md-9">
-				<div class="onglet">
+				<div class="onglet container">
 				    <h2 class="label">Choix des fiches : </h2>
 					<div class="row">
-						<div class="block-fiche px-3 col-md-8"> 
-							<ul class="d-flex justify-content-around">
-								<li><img src="images/page1.PNG"></li>
-								<li><img src="images/page1.PNG"></li>
-								<li><img src="images/page1.PNG"></li>
-								<li><img src="images/page1.PNG"></li>
-								<li><img src="images/page1.PNG"></li>
+						<div class="block-fiche px-3 col-md-8">
+							<ul class="connected-sortable d-flex justify-content-around droppable-area1" id="left-rollbacks">
+
 							</ul>
 						</div>
 					</div>
                     <h2 class="label">Assemblage des fiches : </h2>
 					<div class="row">
-						<div class="block-fiche px-3 col-md-8"> 
-							<ul class="d-flex justify-content-around">
-								<li><img src="images/page1.PNG"></li>
-								<li><img src="images/page1.PNG"></li>
-								<li><img src="images/page1.PNG"></li>
-								<li><img src="images/page1.PNG"></li>
-								<li><img src="images/page1.PNG"></li>
+						<div class="block-fiche px-3 col-md-8">
+							<ul class="connected-sortable d-flex justify-content-around droppable-area2" id="right-rollbacks">
+
+
 							</ul>
 						</div>
 					</div>								
@@ -57,54 +80,73 @@
 </div>
 </section>
 <script src="js/less.min.js" ></script>
-<script src="js/jquery.js" ></script>
 
+<script src="js/jquery.js" ></script>
+<script
+        src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+        integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+        crossorigin="anonymous"></script>
 <script>
-//
-//    $.ajax({
-//        // chargement du fichier externe monfichier-ajax.php
-//        url      : "http://localhost/littleworldBack/fiche/",
-//        cache    : false,
-//        type : "GET",
-//        error    : function(request, error) { // Info Debuggage si erreur
-//            alert("Erreur : responseText: "+request.responseText);
-//        },
-//        success  : function(data) {
-//
-//            data = JSON.parse(data);
-//
-//            console.log(data.length)
-//
-//
-//            for(let i=0; i< data.length; i++){
-//                let li = document.createElement('li');
-//                console.log(data[i])
-//
-//                // li.innerText = data[i].nam_land
-//
-//                let img = document.createElement('img');
-//
-//                img.setAttribute('src', "./images/fiches_pays/"+data[i].nam_img);
-//                img.style.width = '5em'
-//
-//                let text = document.createElement("p")
-//
-//                text.appendChild(document.createTextNode(data[i].nam_land))
-//
-//                text.style.textAlign = "center"
-//
-//                li.appendChild(img);
-//
-//                li.appendChild(text);
-//
-//                li.id = data[i].idt_lnd
-//
-//                document.getElementById('list-fiches').appendChild(li)
-//
-//            }
-//
-//        }
-//    })
+
+   $.ajax({
+       // chargement du fichier externe monfichier-ajax.php
+       url      : "http://localhost/littleworldBack/fiche/",
+       cache    : false,
+       type : "GET",
+       error    : function(request, error) { // Info Debuggage si erreur
+           alert("Erreur : responseText: "+request.responseText);
+       },
+       success  : function(data) {
+
+           data = JSON.parse(data);
+
+           console.log(data.length)
+
+
+           for(let i=0; i< data.length; i++){
+               let li = document.createElement('li');
+               console.log(data[i])
+
+               // li.innerText = data[i].nam_land
+
+               let img = document.createElement('img');
+
+               img.setAttribute('src', "./images/fiches_pays/"+data[i].nam_img);
+               img.style.width = '5em'
+
+               let text = document.createElement("p")
+
+               text.appendChild(document.createTextNode(data[i].nam_land))
+
+               text.style.textAlign = "center"
+
+               li.appendChild(img);
+
+               li.appendChild(text);
+
+               li.id = data[i].idt_lnd
+
+               li.classList.add('draggable-item')
+
+               document.getElementById('left-rollbacks').appendChild(li)
+
+           }
+
+       }
+   });
+
+
+
+
+   $( init );
+
+   function init() {
+       $( ".droppable-area1, .droppable-area2" ).sortable({
+           connectWith: ".connected-sortable",
+           stack: '.connected-sortable ul'
+       }).disableSelection();
+   }
+
 
 </script>
 
