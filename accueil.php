64610bb560cb41d7ee0cf81fb5db9fae987833d2
kioup <mftrookie@example.com>
2018-05-18 15:22:44 +0200

    Page d'accueil complete

--- accueil.php
+++ accueil.php
@@ -21,33 +21,39 @@
         </div>
     </div>
     <div class="row etapes d-flex justify-content-around">
-        <div class="col-md-3">
+        <div class="col-md-4">
             <div class="row">
                 <span class="">1</span>
                 <p class="col-md-10">Dans un premier temps, créer le contenu de ta fiche, choisi la mise la page et télécharge tes images</p>
             </div>
-            <img src="images/picto_crcation-crop-u290.png" alt="">
+            <div class="row">
+                <img src="images/picto_crcation-crop-u290.png" alt="">
+            </div>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
             <div class="row">
                 <span class="">2</span>
                 <p class="col-md-10">Ensuite, retrouve ton ensemble de fiches dans l'onglet "créer un livre". A l'aide de celle-ci, assemble ton livre et jette un oeil dans l'aperçu.</p>
             </div>
-            <img src="images/icone_doc.png" alt="">
-            <img src="images/fleche_doc.png" alt="">
-            <img src="images/picto_bouquin.png" alt="">
+            <div class="row">
+                <img src="images/icone_doc.png" alt="">
+                <img src="images/fleche_doc.png" class="arrow" alt="">
+                <img src="images/picto_bouquin.png" alt="">
+            </div>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
             <div class="row">
                 <span class="">3</span>
                 <p class="col-md-10">Pour finir dans l'onglet bibliothèque, retrouve ton livre et télécharge le avec le lien en PDF (pour une version papier).</p>
             </div>
-            <img src="images/picto_tclcchargement.png" alt="">
+            <div class="row">
+                <img src="images/picto_tclcchargement.png" alt="">
+            </div>
         </div>
     </div>
     <div class="row button">
-        <button>SE CONNECTER</button>
-        <button>INSCRIPTION</button>
+        <a href=""><button>SE CONNECTER</button></a>
+        <a href=""><button>INSCRIPTION</button></a>
     </div>
 </div>
 </section>
