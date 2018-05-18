bf4556f5bead34cf22d3f99afdbc30c05ccc1751
kioup <mftrookie@example.com>
2018-05-18 14:54:39 +0200

    Ajoute de la page d'accueil - 80%

---
+++ accueil.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+	<head lang="fr">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta charset="UTF-8">
+		<title>Littleworld</title>
+		<link rel="stylesheet/less" type="text/css" href="less/stylesFiche.less" />
+		<link rel="stylesheet" type="text/css" href="Bootstrap/css/bootstrap.min.css" />
+	</head>
+<body>
+<section>
+<div class="container accueil">
+    <div class="row  banniere">
+        <div class="col-md-4">
+            <img src="images/planette.png" alt="">
+        </div>
+        <div class="col-md-8">
+            <h1>BIENVENUE CHEZ LITTLE WORLD !</h1>
+            <p>Si tu aimes la mixité et le partage des cultures, tu es ici au bon endoit. Little World est un site internet qui te permet de créer ton propre livre à partir de fiches décrivant des pays du monde entier.</p>
+            <p>Pour créer ton livre, c'est très simple. Voici un tutoriel te permettant de la créer :</p>
+        </div>
+    </div>
+    <div class="row etapes d-flex justify-content-around">
+        <div class="col-md-3">
+            <div class="row">
+                <span class="">1</span>
+                <p class="col-md-10">Dans un premier temps, créer le contenu de ta fiche, choisi la mise la page et télécharge tes images</p>
+            </div>
+            <img src="images/picto_crcation-crop-u290.png" alt="">
+        </div>
+        <div class="col-md-3">
+            <div class="row">
+                <span class="">2</span>
+                <p class="col-md-10">Ensuite, retrouve ton ensemble de fiches dans l'onglet "créer un livre". A l'aide de celle-ci, assemble ton livre et jette un oeil dans l'aperçu.</p>
+            </div>
+            <img src="images/icone_doc.png" alt="">
+            <img src="images/fleche_doc.png" alt="">
+            <img src="images/picto_bouquin.png" alt="">
+        </div>
+        <div class="col-md-3">
+            <div class="row">
+                <span class="">3</span>
+                <p class="col-md-10">Pour finir dans l'onglet bibliothèque, retrouve ton livre et télécharge le avec le lien en PDF (pour une version papier).</p>
+            </div>
+            <img src="images/picto_tclcchargement.png" alt="">
+        </div>
+    </div>
+    <div class="row button">
+        <button>SE CONNECTER</button>
+        <button>INSCRIPTION</button>
+    </div>
+</div>
+</section>
+<script src="js/less.min.js" ></script>
+</body>
+</html>
